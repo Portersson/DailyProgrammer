3f82920f8243cf8c858a57061d290124110bd67d
Jacob Garza <Portersson@gmail.com>
2014-07-16 11:03:24 -0600

    Finished

--- dailyprogrammer_165E.py
+++ dailyprogrammer_165E.py
@@ -2,9 +2,7 @@
 # Challenge 165E: ASCII Game of Life   #
 #           Date: June 13, 2014        #
 ########################################
-# Experimental Branch
-##
-import random, os
+import random, os,time
 
 gridsize = int(input(">"))
 grid = [[0 for x in range(gridsize)] for i in range(gridsize)]
@@ -15,30 +13,28 @@ class Cell(object):
         self.y = y
         self.active = active
         self.activeNeighbors = 9#
-        self.activeOnUpdate = False
-        self.neighbors =   [(x-1,y-1),(x,y-1),(x+1,y-1),
-                            (x-1,y),(x+1,y),
-                            (x-1,y+1),(x,y+1),(x+1,y+1)]
+        self.activeOnUpdate = active
+        self.neighbors =   [[x-1,y-1],[x,y-1],[x+1,y-1],
+                            [x-1,y],[x+1,y],
+                            [x-1,y+1],[x,y+1],[x+1,y+1]]
+        for i in range(8):
+            for i2 in range(2):
+                if self.neighbors[i][i2] == gridsize:
+                    self.neighbors[i][i2] = 0
 
     def iterate(self, grid):
         activeNeighbors = 0
-        for i in range(7):
-            if grid[self.neighbors[i][1]-1][self.neighbors[i][0]-1].active:
+        for i in range(8):
+            if grid[self.neighbors[i][1]][self.neighbors[i][0]].active:
                 activeNeighbors += 1
+                
+        self.activeNeighbors = activeNeighbors #
         if activeNeighbors < 2 or activeNeighbors > 3: self.activeOnUpdate = False
         elif activeNeighbors == 3: self.activeOnUpdate = True
 
     def update(self):
         self.active = self.activeOnUpdate
         
-    def update2(self):
-        ##
-        activeNeighbors = 0
-        for i in range(7):
-            if grid[self.neighbors[i][1]-1][self.neighbors[i][0]-1].active:
-                activeNeighbors += 1
-        self.activeNeighbors = activeNeighbors #
-        ##
 
 def initGrid(gridsize, grid):
     x = 0
@@ -49,7 +45,7 @@ def initGrid(gridsize, grid):
             y+=1
         if y == gridsize:
             break
-        if random.randint(0,8) == 1: state = True
+        if random.randint(0,6) ==1: state = True
         else: state = False
         
         grid[y][x]=Cell(x,y,state,gridsize) #Because console prints line by line (each line being a Y coord), Y must be first
@@ -62,8 +58,9 @@ def drawGrid(grid):
             if o.y != y:
                 print("\n",end='')
                 y=o.y
-            if o.active: print(o.activeNeighbors,end='')
-            else: print("+",end='')
+            if o.active: print('#',end='')
+            else: print(",",end='')
+    print('')
             
 def updateGrid(grid):
     for j in grid: 
@@ -72,12 +69,9 @@ def updateGrid(grid):
     for j in grid:
         for o in j:
             o.update()
-    for j in grid:
-        for o in j:
-            o.update2()
         
 initGrid(gridsize,grid)
 while 1:
     drawGrid(grid)
     updateGrid(grid)
-    input()
+    time.sleep(.08)
